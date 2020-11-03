b94771ba5a3f1a47c4b3db3b71b636f50054d90f
Thomasskolen <73338140+Thomasskolen@users.noreply.github.com>
2020-11-03 09:36:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     basic.showNumber(minode.DHTGetTemperature(ConnName.A1, DHTTemStyle.MINODE_DHT_CELSIUS))
     if (minode.DHTGetTemperature(ConnName.A1, DHTTemStyle.MINODE_DHT_CELSIUS) > 26) {
-        minode.FanControl_1(AnalogConnName.Analog_A0, 100)
+        minode.FanControl_1(AnalogConnName.Analog_A0, 150)
     } else {
         minode.FanControl_1(AnalogConnName.Analog_A0, 0)
     }
